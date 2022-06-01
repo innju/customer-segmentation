cbb0771eee42655129be8abe39404216b9a7a2d3
innju <innjuclover@gmail.com>
2022-06-01 23:45:55 +0800

    Update files
    Updated training file and deployment files. Pickle files, figures and new dataset with predicted are changed based on the updates.

--- customer_segmentation_deploy.py
+++ customer_segmentation_deploy.py
@@ -11,19 +11,30 @@ This script is about the deployment of deep learning model trained.
 from tensorflow.keras.models import load_model
 import os
 import pandas as pd
-import csv
-import numpy as np
 import pickle
-from sklearn.impute import KNNImputer
-
 
 #%% static code
 PATH = os.path.join(os.getcwd() ,'data','new_customers.csv') #load new input data
 MODEL_SAVE_PATH = os.path.join(os.getcwd(),'model_customer_segmentation.h5') 
 # get the path of saved model
 SCALER_SAVE_PATH = os.path.join(os.getcwd(),'mms.pkl')
-OHE_SAVE_PATH = os.path.join(os.getcwd(),'ohe.pkl')# save one hot encoder path
-LABEL_ENCODER_SAVE_PATH = os.path.join(os.getcwd(),'le.pkl')# save scaler path
+OHE_SAVE_PATH = os.path.join(os.getcwd(),'ohe.pkl')# saved one hot encoder path
+# label encoder saved path:
+SAVE_PATH_GENDER = os.path.join(os.getcwd(),'gender.pkl')
+SAVE_PATH_EMARRIED = os.path.join(os.getcwd(),'emarried.pkl')
+SAVE_PATH_GRADUATED = os.path.join(os.getcwd(),'graduated.pkl')
+SAVE_PATH_PROFESSION = os.path.join(os.getcwd(),'profession.pkl')
+SAVE_PATH_SSCORE = os.path.join(os.getcwd(),'sscore.pkl')
+SAVE_PATH_VAR1 = os.path.join(os.getcwd(),'var1.pkl')
+SAVE_PATH_SEGMENTATION = os.path.join(os.getcwd(),'segmentation.pkl')
+SAVE_PATH_KNN_IMPUTER = os.path.join(os.getcwd(),'knn_imputer.pkl')
+NEW_FILE_SAVE_PATH = os.path.join(os.getcwd(),'data','new_customers_predicted.csv')
+
+#%% functions
+def categorical_label_deploy(col_encoder,col_name):
+    le= col_encoder
+    temp = new_input_x[col_name]
+    temp[temp.notnull()]=le.transform(temp[temp.notnull()])
 
 #%% load model
 loaded_model = load_model(MODEL_SAVE_PATH)
@@ -32,63 +43,65 @@ loaded_model.summary() # view the model again
 #%% load pickle files
 scaler = pickle.load(open(SCALER_SAVE_PATH,'rb'))
 ohe = pickle.load(open(OHE_SAVE_PATH,'rb'))
-le = pickle.load(open(LABEL_ENCODER_SAVE_PATH,'rb'))
+# load label encoder path:
+le_gender = pickle.load(open(SAVE_PATH_GENDER,'rb'))
+le_emarried = pickle.load(open(SAVE_PATH_EMARRIED,'rb'))
+le_graduated = pickle.load(open(SAVE_PATH_GRADUATED,'rb'))
+le_profession = pickle.load(open(SAVE_PATH_PROFESSION,'rb'))
+le_sscore = pickle.load(open(SAVE_PATH_SSCORE,'rb'))
+le_var1 = pickle.load(open(SAVE_PATH_VAR1,'rb'))
+le_segmentation = pickle.load(open(SAVE_PATH_SEGMENTATION,'rb'))
+knn_imputer = pickle.load(open(SAVE_PATH_KNN_IMPUTER,'rb'))
 
-#%% deployment with new dataset as input
-new_input_x = pd.read_csv(PATH)
-df = new_input_x
-df.drop(['ID'],axis = 1,inplace=True)
+#%% 1) Load data 
+# load in new dataset
+ori_x = pd.read_csv(PATH)
 #excluding ID column
+new_input_x = (ori_x.loc[:, ori_x.columns != 'ID'])
 
-#%% undone part: preprocessing
-# preprocessing new input data
-# convert string to number
-# since I do the label encoder as below in a complicated way,
-# with the NaN remained, I don't know how should I transform only for the test data
+# 2) Data inspection
+new_input_x.isnull().sum()
+# missing data for Ever_Married(50), Graduated(24),Profession(38),
+# Work_Experience(269), Family_Size(113), Var_1(32), Segmentation (2627)
+new_input_x.info()
+# categorical data identified: 'Gender', 'Ever_Married', 'Graduated', 
+# 'Profession', 'Spending_Score', 'Var_1'
 
-c_data= ['Gender', 'Ever_Married', 'Graduated', 'Profession', 'Spending_Score',
-          'Var_1','Segmentation'] #all categorical data
-for c_data in df.columns:
-    series = df[c_data] # a series of data with name of column
-    df[c_data] = pd.Series(
-       le.transform(series[series.notnull()]),
-       index=series[series.notnull()].index)
-#getting error:ValueError: y contains previously unseen labels: 'Female'
+# 3) data preprocessing
+# convert string to number for categorical data in new input dataset
+categorical_label_deploy(col_encoder=le_gender, col_name='Gender')
+categorical_label_deploy(col_encoder=le_emarried, col_name='Ever_Married')
+categorical_label_deploy(col_encoder=le_graduated, col_name='Graduated')
+categorical_label_deploy(col_encoder=le_profession, col_name='Profession')
+categorical_label_deploy(col_encoder=le_sscore, col_name='Spending_Score')
+categorical_label_deploy(col_encoder=le_var1, col_name='Var_1')
 
+# convert categorical columns above to numeric data type before imputation
+cat_data= ['Gender', 'Ever_Married', 'Graduated', 'Profession', 'Spending_Score',
+          'Var_1'] #all categorical data
+new_input_x[cat_data] = new_input_x[cat_data].apply(pd.to_numeric, errors='coerce')
 
-# If the step above is success,
-# then can proceed deal with NaN value in the new dataset using KNN imputer
-imputer = KNNImputer()
+# recheck data type to ensure successful data conversion
+new_input_x.info()
+# no more object data type
+
+# then can proceed to deal with NaN value in the new dataset using KNN imputer
 # by default, n_neighbors=5, metric='nan_euclidean'
-df['Ever_Married']= imputer.fit_transform(np.expand_dims(df['Ever_Married'],-1))
-df['Graduated'] = imputer.fit_transform(np.expand_dims(df['Graduated'],-1))
-df['Profession']= imputer.fit_transform(np.expand_dims(df['Profession'],-1))
-df['Family_Size'] = imputer.fit_transform(np.expand_dims(df['Family_Size'],-1))
-df['Work_Experience']= imputer.fit_transform(np.expand_dims(df['Work_Experience'],-1))
-df['Var_1']= imputer.fit_transform(np.expand_dims(df['Var_1'],-1))
-
-df = scaler.transform(df) #transform the data using min max scaler imported
-
-#%% undone part: prediction
-
-# predicted
-predicted = loaded_model.predict(df)
-
-# need to do inverse transform to get back the category name for segmentation
-# column first
-# and the concatenate the original raw data and predicted dataset together
-# before save it to new csv file
-predicted = pd.DataFrame(predicted)
-new_input_x2 = new_input_x.iloc[:,0:10] #get column from ID to Var_1
-overall = pd.concat([new_input_x2, predicted], axis = 1)
-
-# save the info as new csv file
-filename='new_customers_predicted.csv'
-with open(filename,'w') as csvfile:
-    csvwriter = csv.writer(csvfile) 
-    
-    
+new_input_x2 = knn_imputer.transform(new_input_x)
+new_input_x2 = pd.DataFrame(new_input_x2)
+# transform the data using min max scaler imported
+# get the features by exclude the last column (target: segmentation)
+X_data = scaler.transform(new_input_x2.iloc[:,:-1])
 
 
-
+#%% Prediction
+predicted = loaded_model.predict(X_data)
+# need to do inverse transform to get back the number labelled for segmentation
+predicted2 = ohe.inverse_transform(predicted)
+print(predicted2.dtype) #float
+# change to int in order to perform inverse and transform using label encoder
+predicted2 = le_segmentation.inverse_transform(predicted2.astype(int))
+# replace segmentation column from original data with prediction
+ori_x['Segmentation'] = predicted2
+ori_x.to_csv(NEW_FILE_SAVE_PATH,index=False)
 
--- customer_segmentation_train.py
+++ customer_segmentation_train.py
@@ -24,10 +24,10 @@ from tensorflow.keras import Input,Sequential
 from sklearn.metrics import confusion_matrix,classification_report
 from sklearn.preprocessing import OneHotEncoder
 import pickle
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #%% Static code
-
 PATH = os.path.join(os.getcwd(),'data','train.csv') #load data
 MODEL_SAVE_PATH = os.path.join(os.getcwd(),'model_customer_segmentation.h5') #save model
 LOG_SAVE_PATH = os.path.join(os.getcwd(),'log_customer_segmentation') 
@@ -35,9 +35,25 @@ log_customer_segmentation = os.path.join(
     LOG_SAVE_PATH,datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
 SCALER_SAVE_PATH = os.path.join(os.getcwd(),'mms_scaler.pkl')# save scaler path
 OHE_SAVE_PATH = os.path.join(os.getcwd(),'ohe.pkl')# save encoder path
-LABEL_ENCODER_SAVE_PATH = os.path.join(os.getcwd(),'le.pkl')# save encoder path
+# save label encoder path:
+SAVE_PATH_GENDER = os.path.join(os.getcwd(),'gender.pkl')
+SAVE_PATH_EMARRIED = os.path.join(os.getcwd(),'emarried.pkl')
+SAVE_PATH_GRADUATED = os.path.join(os.getcwd(),'graduated.pkl')
+SAVE_PATH_PROFESSION = os.path.join(os.getcwd(),'profession.pkl')
+SAVE_PATH_SSCORE = os.path.join(os.getcwd(),'sscore.pkl')
+SAVE_PATH_VAR1 = os.path.join(os.getcwd(),'var1.pkl')
+SAVE_PATH_SEGMENTATION = os.path.join(os.getcwd(),'segmentation.pkl')
+SAVE_PATH_KNN_IMPUTER = os.path.join(os.getcwd(),'knn_imputer.pkl')
 
 #%% Function defined
+# save label encoder
+def categorical_label(col_name,SAVE_PATH):
+    le= LabelEncoder()
+    temp = df[col_name]
+    temp[temp.notnull()]=le.fit_transform(temp[temp.notnull()])
+    # choose to fit and transform for data that is not null
+    pickle.dump(le,open(SAVE_PATH,'wb'))
+
 # generate classification report
 def report_generation(x_test,y_test):
     pred_x = model.predict(x_test)
@@ -76,26 +92,38 @@ df.describe().T
 df.drop(['ID'],axis = 1,inplace=True)
 
 # need to change categorical data to numerical data
-# convert string to number
-le = LabelEncoder()
-encoders = dict()
-c_data= ['Gender', 'Ever_Married', 'Graduated', 'Profession', 'Spending_Score',
-         'Var_1','Segmentation'] #all categorical data
-for c_data in df.columns:
-    series = df[c_data] # a series of data with name of column
-    label_encoder = LabelEncoder()
-    df[c_data] = pd.Series(
-        label_encoder.fit_transform(series[series.notnull()]),
-        index=series[series.notnull()].index)
-    encoders[c_data] = label_encoder
-print(encoders)
-# save label encoding
-lefile='le.pkl'
-pickle.dump(label_encoder,open(lefile,'wb'))
+# convert string to number using label encoder
 
-# remain NaN in the dataset 
-# references:https://localcoder.org/labelencoder-that-keeps-missing-values-as-nan
-print(df)
+#%% Label encoder updated
+# be careful, if there is NaN in the data
+# it will treat the NaN as certain label
+# because NaN is recognized as float in label encoder
+# hence, have to tell label encoder to remain the NaN in dataset
+# save the label encoder for each categorical column one by one
+# so that it can be used in deployment file later (for preprocessing new input data)
+# since the column name is set in the function, no need to write df['Gender']= ...anymore
+categorical_label(col_name='Gender',SAVE_PATH=SAVE_PATH_GENDER)
+categorical_label(col_name='Ever_Married',SAVE_PATH=SAVE_PATH_EMARRIED)
+categorical_label(col_name='Graduated',SAVE_PATH=SAVE_PATH_GRADUATED)
+categorical_label(col_name='Profession',SAVE_PATH=SAVE_PATH_PROFESSION)
+categorical_label(col_name='Spending_Score',SAVE_PATH=SAVE_PATH_SSCORE)
+categorical_label(col_name='Var_1',SAVE_PATH=SAVE_PATH_VAR1)
+categorical_label(col_name='Segmentation',SAVE_PATH=SAVE_PATH_SEGMENTATION)
+
+# convert categorical columns above to numeric data type
+c_data= ['Gender', 'Ever_Married', 'Graduated', 'Profession', 'Spending_Score',
+          'Var_1','Segmentation'] #all categorical data
+df[c_data] = df[c_data].apply(pd.to_numeric, errors='coerce')
+
+# recheck data type to ensure successful data conversion
+df.info()
+
+# check the data distribution 
+sns.pairplot(df,hue='Segmentation',palette='colorblind')
+plt.show()
+# no obvious patterns observed for 4 categories of segmentation
+# could be a hint to poor performance of deep learning model
+#%%
 
 # deal with missing data
 # check the null value again
@@ -105,63 +133,68 @@ df.isnull().sum()
 # replace missing data using KNN imputer
 imputer = KNNImputer()
 # by default, n_neighbors=5, metric='nan_euclidean'
-df['Ever_Married']= imputer.fit_transform(np.expand_dims(df['Ever_Married'],-1))
-df['Graduated'] = imputer.fit_transform(np.expand_dims(df['Graduated'],-1))
-df['Profession']= imputer.fit_transform(np.expand_dims(df['Profession'],-1))
-df['Family_Size'] = imputer.fit_transform(np.expand_dims(df['Family_Size'],-1))
-df['Work_Experience']= imputer.fit_transform(np.expand_dims(df['Work_Experience'],-1))
-df['Var_1']= imputer.fit_transform(np.expand_dims(df['Var_1'],-1))
-
+# do not fit and transform certain column only
+# because KNN will takes the properties of the entire dataset to impute
+df2 = imputer.fit_transform(df)
+# save knnn imputer to pickle file
+knn='knn_imputer.pkl'
+pickle.dump(imputer,open(knn,'wb'))
+# rename columns name of df2
+df2 = pd.DataFrame(df2,columns=[df.columns])
 
 # recheck data
-df.describe().T
+df2.isnull().sum() 
+# no more missing data
+
 
 # 4) Feature selection
 None
 
 # 5) Data preprocessing
-X= df.loc[:, df.columns != 'Segmentation'] # features
-y = df['Segmentation'] #target
+X= df2.loc[:, df.columns != 'Segmentation'] # features
+y = df2['Segmentation'] #target
 print(X.shape) #(8068,9)
-print(y.shape) #(8068,)
-
-
-# train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=12)
+print(y.shape) #(8068,1)
 
 # data scaling using MinMaxScaler
-# fit and transfrom for testing dataset
-# transform only for testing dataset
 mms= MinMaxScaler()
-x_train = mms.fit_transform(x_train)
-x_test = mms.transform(x_test)
-print(x_train.shape) #(5647,9)
-print(x_test.shape) #(2421,9)
+X = mms.fit_transform(X)
 # save scaler to pickle file
 mms_scaler='mms.pkl'
 pickle.dump(mms,open(mms_scaler,'wb'))
 
+
 enc = OneHotEncoder(sparse=False)
-y_train = enc.fit_transform(np.expand_dims(y_train,axis=-1)) # y train
-y_test = enc.transform(np.expand_dims(y_test,axis=-1)) # y test
+y = enc.fit_transform(y)
 # save one hot encoder to pickle file
 ohe='ohe.pkl'
 pickle.dump(enc,open(ohe,'wb'))
+
+
+# train_test_split have to be the very last step after any data preprocessing
+x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
+
 
 #%% Train model
 # sequential
 model = Sequential(name=('customer_segmentation'))
 model.add(Input(shape=(9))) #input layer 1
-model.add(Dense(100,activation='relu')) #hidden layer 1
+model.add(Dense(64,activation='relu')) #hidden layer 1
 model.add(BatchNormalization()) 
 model.add(Dropout(0.2))
-model.add(Dense(82,activation= 'relu')) #hidden layer 2
+model.add(Dense(64,activation= 'relu')) #hidden layer 2
 model.add(BatchNormalization())
 model.add(Dropout(0.2))
-model.add(Dense(62,activation= 'relu')) #hidden layer 3
+model.add(Dense(64,activation= 'relu')) #hidden layer 3
 model.add(BatchNormalization())
 model.add(Dropout(0.2))
-model.add(Dense(32,activation= 'relu')) # hidden layer 4
+model.add(Dense(64,activation= 'relu')) # hidden layer 4
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(32,activation= 'relu')) # hidden layer 5
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+model.add(Dense(32,activation= 'relu')) # hidden layer 6
 model.add(BatchNormalization())
 model.add(Dropout(0.2))
 model.add(Dense(4,activation='softmax')) #output layer
@@ -178,8 +211,8 @@ tensorboard_callback = TensorBoard(log_dir= log_customer_segmentation, histogram
 
 # early stopping callback
 # to deal with overfitting
-# below here ask it to refer to val_loss, stop training after overfit by 10 times (refer to patience)
-early_stopping_callback = EarlyStopping(monitor='val_loss',patience=6)
+# below here ask it to refer to val_loss, stop training after overfit by 4 times (refer to patience)
+early_stopping_callback = EarlyStopping(monitor='val_loss',patience=4)
 
 # train the model
 hist = model.fit(x_train,y_train, epochs=200, validation_data=(x_test,y_test),
@@ -187,20 +220,38 @@ hist = model.fit(x_train,y_train, epochs=200, validation_data=(x_test,y_test),
 
 # to show how many epochs it run
 len(hist.history['val_loss'])
-# it stopped at epoch 26
+# it stopped at epoch 36
 
 #generate classification report
 report_generation(x_test, y_test)
 # low accuracy obtained, 54%
-# reasons not able to achieves high accuracy because
-# lack of feature selection 
-# functional api might perform better since there is multiple input and
-# multiple output in this analysis
+
+#%% reasons not able to achieves high accuracy:
+# there is no pattern in the dataset
+# can try to use unsupervised learning model to see if it can show any clusters or not
+# if there is no pattern ==> deep learning fails to work
+from sklearn.cluster import KMeans
+# use elbow method to find the optimal number of cluster to cluster this data
+cs = []
+for i in range(1,6):
+    kmeans = KMeans(n_clusters = i,random_state = 0)
+    kmeans.fit(X)
+    cs.append(kmeans.inertia_)
+plt.plot(range(1,6), cs)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('CS')
+plt.show()
+# based on the graph, 2 is the ideal number of cluster to cluster this dataset
+# however, high kmeans inertia obtained: 6929.94
+# if k=4, high kmeans inertia obtained: 4869.43
+# as a conclusion, this dataset is not able to contribute obvious patterns in
+# categorize 4 categories of segmentation
+# thus, it resulted poor performance in the deep learning model
 
 
 #%% Save model
 # saving model
 model.save(MODEL_SAVE_PATH)
-
 
 
